0c4c0c5e53afa5eee8fc25665fd84e76b0f5d828
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-24 10:10:53 -0600

    finishes lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
-# your code goes here!
+class Anagram: 
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, input_list):
+        list = []
+        word_string = [letter for letter in self.word]
+        sorted_word = sorted(word_string)
+        for word in input_list:
+            compare = [letter for letter in word]
+            sorted_compare = sorted(compare)
+            if sorted_word == sorted_compare:
+                list.append(word)
+        return list
